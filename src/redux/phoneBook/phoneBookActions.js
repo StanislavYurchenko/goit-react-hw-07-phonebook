010924d70936295accf9a0c24c1d0f44aa82630a
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-01-26 13:53:33 +0200

    ready

--- src/redux/phoneBook/phoneBookActions.js
+++ src/redux/phoneBook/phoneBookActions.js
@@ -1,23 +1,3 @@
 import { createAction } from '@reduxjs/toolkit';
 
 export const onChangeFilter = createAction('phoneBook/onChangeFilter');
-
-export const addContactRequest = createAction('phoneBook/addContactRequest');
-export const addContactSuccess = createAction('phoneBook/addContactSuccess');
-export const addContactError = createAction('phoneBook/addContactError');
-
-// export const removeContactRequest = createAction(
-//   'phoneBook/removeContactRequest',
-// );
-// export const removeContactSuccess = createAction(
-//   'phoneBook/removeContactSuccess',
-// );
-// export const removeContactError = createAction('phoneBook/removeContactError');
-
-// export const fetchContactsRequest = createAction(
-//   'phoneBook/fetchContactRequest',
-// );
-// export const fetchContactsSuccess = createAction(
-//   'phoneBook/fetchContactSuccess',
-// );
-// export const fetchContactsError = createAction('phoneBook/fetchContactError');
